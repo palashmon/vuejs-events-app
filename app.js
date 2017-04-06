818d2356200d85a716ff1bd743dfd795792e53e4
Palash Mondal <palashm@live.in>
2017-04-06 21:19:38 +0530

    Created a new Vue instance, added html markup

---
+++ app.js
@@ -0,0 +1,17 @@
+// app.js
+// Creating a Vue Instance    
+new Vue({
+
+  // We want to target the div with an id of 'events'
+  el: '#events',
+
+  // Here we can register any values or collections that hold data
+  // for the application
+  data: {},
+
+  // Anything within the ready function will run when the application loads
+  ready: function() {},
+
+  // Methods we want to use in our application are registered here
+  methods: {}
+});
